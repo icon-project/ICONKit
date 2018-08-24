7744aa917ccb4da3a7708f02d2b7607a11ead1ac
a1ahn <jeonghwan.ahn@icon.foundation>
2018-08-24 11:41:11 +0900

    Initial Commit

---
+++ ICONKit/ICONKit.h
@@ -0,0 +1,19 @@
+//
+//  ICONKit.h
+//  ICONKit
+//
+//  Created by a1ahn on 24/08/2018.
+//  Copyright © 2018 ICON Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ICONKit.
+FOUNDATION_EXPORT double ICONKitVersionNumber;
+
+//! Project version string for ICONKit.
+FOUNDATION_EXPORT const unsigned char ICONKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICONKit/PublicHeader.h>
+
+
