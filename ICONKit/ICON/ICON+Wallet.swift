f75b8ec65b14fcf2998de71a6bfbc714f320c5b9
a1ahn <jeonghwan.ahn@icon.foundation>
2019-01-07 17:13:57 +0900

    Remove keystore

--- ICONKit/ICON/ICON+Wallet.swift
+++ ICONKit/ICON/ICON+Wallet.swift
@@ -24,49 +24,23 @@ import CryptoSwift
 extension ICON {
     
     open class Wallet: SECP256k1, Cipher {
-        public var keystore: ICON.Keystore?
+        public var privateKey: String?
         public var address: String? {
-            guard let keystore = self.keystore else { return nil }
+            guard let key = self.privateKey, let publicKey = self.createPublicKey(privateKey: key) else { return nil }
             
-            return keystore.address
-        }
-        public var rawData: Data? {
-            guard let keystore = self.keystore else { return nil }
-            do {
-                let encoder = JSONEncoder()
-                return try encoder.encode(keystore)
-            } catch {
-                
-            }
-            return nil
+            return self.makeAddress(key, publicKey)
         }
         
-        init() {}
+        private init() {}
     }
     
 }
 
 extension ICON.Wallet {
     
-    public convenience init(keystore: ICON.Keystore) {
+    public convenience init(privateKey: String) {
         self.init()
-        self.keystore = keystore
-    }
-    
-    public convenience init(privateKey: String?, password: String) {
-        self.init()
-        guard let keystore = self.createKeystore(privateKey, password) else { return }
-        self.keystore = keystore
-    }
-    
-    public convenience init?(rawData: Data) {
-        do {
-            let decoder = JSONDecoder()
-            let keystore = try decoder.decode(ICON.Keystore.self, from: rawData)
-            self.init(keystore: keystore)
-        } catch {
-            return nil
-        }
+        self.privateKey = privateKey
     }
     
     private func generatePrivateKey() -> String {
@@ -82,103 +56,6 @@ extension ICON.Wallet {
         return key.sha3(.sha256)
     }
     
-    private func createKeystore(_ privateKey: String?, _ password: String) -> ICON.Keystore? {
-        do {
-            var key: String
-            if let prvKey = privateKey {
-                key = prvKey
-            } else {
-                key = generatePrivateKey()
-            }
-            
-            guard let publicKey = createPublicKey(privateKey: key), let pubData = publicKey.hexToData() else {
-                return nil
-            }
-            
-            let address = makeAddress(privateKey, pubData)
-            
-            let saltCount = 32
-            var randomBytes = Array<UInt8>(repeating: 0, count: saltCount)
-            let err = SecRandomCopyBytes(kSecRandomDefault, saltCount, &randomBytes)
-            if err != errSecSuccess { return nil }
-            let salt = Data(bytes: randomBytes)
-            
-            // HASH round
-            let round = 16384
-            
-            guard let encKey = pbkdf2SHA256(password: password, salt: salt, keyByteCount: PBE_DKLEN, round: round) else {
-                return nil
-            }
-            
-            let result = try encrypt(devKey: encKey, data: key.hexToData()!, salt: salt)
-            let kdfParam = ICON.Keystore.KDF(dklen: PBE_DKLEN, salt: salt.toHexString(), c: round, prf: "hmac-sha256")
-            let crypto = ICON.Keystore.Crypto(ciphertext: result.cipherText, cipherparams: ICON.Keystore.CipherParams(iv: result.iv), cipher: "aes-128-ctr", kdf: "pbkdf2", kdfparams: kdfParam, mac: result.mac)
-            let keyStore = ICON.Keystore(address: address, crypto: crypto)
-            keyStore.coinType = "icx"
-            
-            return keyStore
-        } catch {
-            return nil
-        }
-    }
-    
-    public func extractPrivateKey(password: String) throws -> String {
-        guard let keystore = self.keystore else { throw ICError.empty }
-        
-        if keystore.crypto.kdf == "pbkdf2" {
-            guard let enc = keystore.crypto.ciphertext.hexToData(),
-                let iv = keystore.crypto.cipherparams.iv.hexToData(),
-                let salt = keystore.crypto.kdfparams.salt.hexToData(),
-                let count = keystore.crypto.kdfparams.c else { throw ICError.malformed }
-            
-            guard let devKey = pbkdf2SHA256(password: password, salt: salt, keyByteCount: ICON.Util.PBE_DKLEN, round: count) else { throw ICError.decrypt }
-            
-            let decrypted = try decrypt(devKey: devKey, enc: enc, dkLen: ICON.Util.PBE_DKLEN, iv: iv)
-            let publicKey = createPublicKey(privateKey: decrypted.decryptText)
-            let newAddress = makeAddress(decrypted.decryptText, publicKey!)
-            
-            if newAddress == keystore.address {
-                return decrypted.decryptText
-            }
-            
-            throw ICError.invalid(.privateKey)
-            
-        } else if keystore.crypto.kdf == "scrypt" {
-            guard let n = keystore.crypto.kdfparams.n,
-                let p = keystore.crypto.kdfparams.p,
-                let r = keystore.crypto.kdfparams.r,
-                let iv = keystore.crypto.cipherparams.iv.hexToData(),
-                let cipherText = keystore.crypto.ciphertext.hexToData(),
-                let salt = keystore.crypto.kdfparams.salt.hexToData()
-                else { throw ICError.malformed }
-            
-            guard let devKey = scrypt(password: password, saltData: salt, dkLen: keystore.crypto.kdfparams.dklen, N: n, R: r, P: p) else { throw ICError.decrypt }
-            let decryptionKey = devKey[0...15]
-            guard let aesCipher = try? AES(key: decryptionKey.bytes, blockMode: CTR(iv: iv.bytes), padding: .noPadding) else { throw ICError.decrypt }
-            guard let decryptedBytes = try? aesCipher.decrypt(cipherText.bytes) else { throw ICError.decrypt }
-            let decrypted = Data(bytes: decryptedBytes).toHexString()
-            
-            let publicKey = createPublicKey(privateKey: decrypted)
-            let newAddress = makeAddress(decrypted, publicKey!)
-            
-            if newAddress == keystore.address {
-                return decrypted
-            }
-            
-            throw ICError.invalid(.privateKey)
-        }
-        
-        throw ICError.invalid(.notSupported)
-    }
-    
-    public func changePassword(current: String, new: String) throws {
-        let prvKey = try extractPrivateKey(password: current)
-        
-        guard let keystore = createKeystore(prvKey, new) else { throw ICError.encrypt }
-        self.keystore = keystore
-        
-    }
-    
     /// Signing
     ///
     /// - Parameters:
@@ -187,9 +64,7 @@ extension ICON.Wallet {
     /// - Returns: Signed.
     /// - Throws: exceptions
     public func getSignature(password: String, data: Data) throws -> String {
-        
-        let privateKey = try self.extractPrivateKey(password: password)
-        
+        guard let privateKey = self.privateKey else { throw ICError.empty }
         let hash = data.sha3(.sha256)
         
         let sign = try signECDSA(hashedMessage: hash, privateKey: privateKey)
