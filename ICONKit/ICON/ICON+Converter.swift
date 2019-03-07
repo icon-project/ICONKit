2bd17514caf7929e54b072b8372065f951fc00b3
이승연 <adie0423@gmail.com>
2019-03-07 16:47:08 +0900

    Add converter

--- ICONKit/ICON/ICON+Converter.swift
+++ ICONKit/ICON/ICON+Converter.swift
@@ -16,3 +16,53 @@
  */
 
 import Foundation
+import BigInt
+
+public enum Unit: Int {
+    case icx = 18
+    case gLoop = 9
+    case loop = 0
+}
+
+public struct Converter {
+    
+}
+extension Converter {
+    /// Convert ICX or gLoop value to loop.
+    ///
+    /// - Parameters:
+    ///   - value: The value convert to loop.
+    ///   - unit: The Unit of value. `default = .icx`.
+    /// - Returns: A value converted to loop.
+    public static func convertToLoop(value: BigUInt, unit: Unit = .icx) -> BigUInt {
+        let base: BigInt = 10
+        let indices: BigUInt = BigUInt(base.power(unit.rawValue))
+        switch unit {
+        case .gLoop:
+            let result = value.multiplied(by: indices)
+            return result
+        case .icx:
+            let result = value.multiplied(by: indices)
+            return result
+        default:
+            return value
+        }
+    }
+}
+
+extension BigUInt {
+    /// Convert BigUInt to hexString.
+    public var toHexString: String? {
+        return "0x" + String(self, radix: 16)
+    }
+}
+
+extension Data {
+    /// Convert hexData to BigUInt.
+    public var toBigUInt: BigUInt {
+        return BigUInt(self)
+    }
+
+}
+
+
