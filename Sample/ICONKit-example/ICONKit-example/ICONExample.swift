d06c6fe089eb02cf9962ebc1fb0545f16cf8debb
sweepty <adie0423@gmail.com>
2019-05-28 15:31:21 +0900

    Update ICONExample - Add estiamteStep example source - Drop test code

--- Sample/ICONKit-example/ICONKit-example/ICONExample.swift
+++ Sample/ICONKit-example/ICONKit-example/ICONExample.swift
@@ -22,86 +22,107 @@ class ICONExample {
     let governanceAddress = "cx0000000000000000000000000000000000000001"
     
     init() {
-        // Mainnet
+        // 1. Mainnet
 //        let provider = "https://ctz.solidwallet.io/api/v3"
 //        iconService = ICONService(provider: provider, nid: "0x1")
         
-        // Testnet for DApps
+        // 2. Testnet for DApps (Yeouido)
 //        let provider = "https://bicon.net.solidwallet.io/api/v3"
 //        iconService = ICONService(provider: provider, nid: "0x3")
         
-        // Testnet for Exchanger
+        // 3. Testnet for Exchanger (Euljiro)
         let provider = "https://test-ctz.solidwallet.io/api/v3"
         iconService = ICONService(provider: provider, nid: "0x2")
-        
-        // If you have a wallet which has some ICX for test, use loadWallet with your private key
-//        createWallet()
-//        loadWallet(privateKey: yourPrivateKey)
-        
-//        asyncBlock()
-//        asyncSupply()
-//        asyncTransaction()
-//        getLastBlock()
-//        getBlockByHash()
-//        getBlockByHeight(height: 54156)
-//        getTransactionByHash()
-//        getTransactionResult()
-//        getScoreAPI()
-//        getGovernanceScoreAPI()
-//        getBalance()
-//        sendTransaction()
-//        getTotalSupply()
-//        getDefaultStepCost()
-        
-        test()
     }
     
-    func asyncBlock() {
-        iconService.getLastBlock().async { (result) in
-            switch result {
-            case .success(let val):
-                print(val.blockHash)
-            case .failure(let err):
-                print(err.errorDescription)
-            }
-            
-        }
-    }
-    func asyncSupply(_ completion: @escaping(BigUInt) -> Void) {
-        iconService.getTotalSupply().async { (result) in
-            switch result {
-            case .success(let val):
-                print(val)
-                completion(val)
-            case .failure(let err):
-                print(err.errorDescription)
-//                completion(err.errorDescription)
-            }
-        }
-    }
-    func asyncTransaction() {
-        let message = MessageTransaction()
-            .from(fromAddress)
-            .to(toAddress)
-            .stepLimit(BigUInt(15000000))
-            .nonce("0x1")
-            .nid(self.iconService.nid)
-            .message("Hello, ICON!")
+    func getLastBlock(_ completion: @escaping(Response.Block) -> Void) {
+        let response = iconService.getLastBlock().execute()
         
-        do {
-            let signed = try SignedTransaction(transaction: message, privateKey: yourPrivateKey)
-            
-            let request = iconService.sendTransaction(signedTransaction: signed)
-            request.async { (result) in
-                switch result {
-                case .success(let val):
-                    print(val)
-                case .failure(let err):
-                    print(err.errorDescription)
-                }
+        switch response {
+        case .success(let result):
+            print(result.blockHash)
+            completion(result)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    func getBlockByHeight(height: UInt64, _ completion: @escaping(Response.Block) -> Void) {
+        let response = iconService.getBlock(height: height).execute()
+        
+        switch response {
+        case .success(let result):
+            print(result.blockHash)
+            completion(result)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    func getBlockByHash() {
+        let response = iconService.getBlock(hash: "0x4e468893e56ef2cd75eb82cc4ff7026bf2baf72a47c0355a7a94da523af7aa3f").execute()
+        
+        switch response {
+        case .success(let result):
+            print(result.blockHash)
+        case .failure(let err):
+            print(err.localizedDescription)
+        }
+    }
+    
+    func getBalance() {
+        let response = iconService.getBalance(address: "hx9043346dbaa72bca42ecec6b6e22845a4047426d").execute()
+        switch response {
+        case .success(let data):
+            print(data)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    
+    func getScoreAPI() {
+        let result = iconService.getScoreAPI(scoreAddress: scoreAddress).execute()
+        switch result {
+        case .success(let data):
+            for api in data {
+                print("name: \(api.name) , type: \(api.type) , input: \(api.inputs) , output: \(String(describing: api.outputs)) , payable: \(String(describing: api.payable)) , readonly: \(String(describing: api.readonly))")
             }
-        } catch {
-            print(error)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    func getTotalSupply() {
+        let response = iconService.getTotalSupply().execute()
+        
+        switch response {
+        case .success(let data):
+            print(data)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    func getTransactionResult() {
+        let response = iconService.getTransactionResult(hash: "0x1155fd70a265db32f2fa307dd64fa74820dfada2969da6ca7ea00242e319e067").execute()
+        
+        switch response {
+        case .success(let result):
+            print(result.blockHash)
+        case .failure(let err):
+            print(err)
+        }
+    }
+    
+    func getTransactionByHash() {
+        let response = iconService.getTransaction(hash: "0x1155fd70a265db32f2fa307dd64fa74820dfada2969da6ca7ea00242e319e067").execute()
+        
+        switch response {
+        case .success(let result):
+            print(result.blockHash)
+        case .failure(let err):
+            print(err)
         }
     }
     
@@ -151,48 +172,37 @@ class ICONExample {
         case .success(let val):
             print(val.apiCall)
         case .failure(let err):
-            print(err.errorDescription)
+            print(err)
         }
     }
     
-    func sendTransaction() {
-        // coin transfer
+    // Send ICX
+    func sendICX() {
+        // ICX transfer
         let coinTransfer = Transaction()
             .from(fromAddress)
             .to(toAddress)
             .value(BigUInt(15000000))
-            .stepLimit(BigUInt(1000000))
             .nid(self.iconService.nid)
-            .nonce("0x1")
         
-        // SCORE function call
-        let call = CallTransaction()
-            .from(fromAddress)
-            .to(scoreAddress)
-            .value(BigUInt(15000000))
-            .stepLimit(BigUInt(1000000))
-            .nid(self.iconService.nid)
-            .nonce("0x1")
-            .method("transfer")
-            .params(["_to": "hx2e26d96bd7f1f46aac030725d1e302cf91420458", "_value": "0x29a2241af62c0000"])
+        // Estimate step cost
+        let request = iconService.estimateStep(transaction: coinTransfer)
+        let response = request.execute()
+
+        if let estimatedStepCost = try? response.get() {
+            print("Estimated step cost: \(estimatedStepCost)")
+            coinTransfer.stepLimit(estimatedStepCost)
+        }
         
-        // Message transfer
-        let message = MessageTransaction()
-            .from(fromAddress)
-            .to(toAddress)
-            .stepLimit(BigUInt(15000000))
-            .nonce("0x1")
-            .nid(self.iconService.nid)
-            .message("Hello, ICON!")
-        
+        // Send transaction
         do {
-            let signed = try SignedTransaction(transaction: message, privateKey: yourPrivateKey)
-            
+            let signed = try SignedTransaction(transaction: coinTransfer, privateKey: yourPrivateKey)
+
             let request = iconService.sendTransaction(signedTransaction: signed)
             let response = request.execute()
             switch response {
             case .success(let result):
-                print("tx result - \(result)")
+                print("ICX tx result - \(result)")
 
             case .failure(let error):
                 print(error)
@@ -202,126 +212,136 @@ class ICONExample {
         }
     }
     
-    func getLastBlock(_ completion: @escaping(Response.Block) -> Void) {
-        let response = iconService.getLastBlock().execute()
+    // Score function call
+    func scoreCallTransaction() {
+        let call = CallTransaction()
+            .from(fromAddress)
+            .to(scoreAddress)
+            .nid(self.iconService.nid)
+            .method("get_frozen_balance")
+            .params(["owner": "hx8a69626aadca96f15ebc649ced46b92e8785d034"])
         
-        switch response {
-        case .success(let result):
-            print(result.blockHash)
-            completion(result)
-        case .failure(let err):
-            print(err.errorDescription)
+        // Estimate step cost
+        let response = iconService.estimateStep(transaction: call).execute()
+        do {
+            let estiamteCost = try response.get()
+            call.stepLimit(estiamteCost)
+        } catch {
+            print(error)
         }
-    }
-    
-    func getBlockByHeight(height: UInt64, _ completion: @escaping(Response.Block) -> Void) {
-        let response = iconService.getBlock(height: height).execute()
         
-        switch response {
-        case .success(let result):
-            print(result.blockHash)
-            completion(result)
-        case .failure(let err):
-            print(err.errorDescription)
-        }
-    }
-    
-    func getBlockByHash() {
-        let response = iconService.getBlock(hash: "0x4e468893e56ef2cd75eb82cc4ff7026bf2baf72a47c0355a7a94da523af7aa3f").execute()
-        
-        switch response {
-        case .success(let result):
-            print(result.blockHash)
-        case .failure(let err):
-            print(err.localizedDescription)
-        }
-    }
-    
-    func getBalance() {
-        let response = iconService.getBalance(address: "hx9043346dbaa72bca42ecec6b6e22845a4047426d").execute()
-        switch response {
-        case .success(let data):
-            print(data)
-        case .failure(let err):
-            print(err)
-        }
-    }
-    
-
-    func getScoreAPI() {
-        let result = iconService.getScoreAPI(scoreAddress: scoreAddress).execute()
-        switch result {
-        case .success(let data):
-            for api in data {
-                print("name: \(api.name) , type: \(api.type) , input: \(api.inputs) , output: \(String(describing: api.outputs)) , payable: \(String(describing: api.payable)) , readonly: \(String(describing: api.readonly))")
+        // Send transaction
+        do {
+            let signed = try SignedTransaction(transaction: call, privateKey: yourPrivateKey)
+            
+            let request = iconService.sendTransaction(signedTransaction: signed)
+            let response = request.execute()
+            
+            switch response {
+            case .success(let result):
+                print("SCORE tx result - \(result)")
+            case .failure(let error):
+                print(error)
             }
-        case .failure(let err):
-            print(err)
+        } catch {
+            print(error)
         }
     }
     
-    func getTotalSupply() {
-        let response = iconService.getTotalSupply().execute()
+    // Send message transaction
+    func sendMessageTransaction() {
+        let message = MessageTransaction()
+            .from(fromAddress)
+            .to(toAddress)
+            .nid(self.iconService.nid)
+            .nonce("0x1")
+            .message("Hello, ICON!")
         
-        switch response {
-        case .success(let data):
-            print(data)
-        case .failure(let err):
-            print(err)
-        }
-    }
-    
-    func getTransactionResult() {
-        let response = iconService.getTransactionResult(hash: "0x1155fd70a265db32f2fa307dd64fa74820dfada2969da6ca7ea00242e319e067").execute()
+        let messageRequest = iconService.estimateStep(transaction: message)
+        let messageResponse = messageRequest.execute()
 
-        switch response {
-        case .success(let result):
-            print(result.blockHash)
-        case .failure(let err):
-            print(err.errorDescription)
+        switch messageResponse {
+        case .success(let value):
+            print("stepCost: \(value)")
+            message.stepLimit(value)
+        case .failure(let error):
+            print(error)
+        }
+        
+        do {
+            let signed = try SignedTransaction(transaction: message, privateKey: self.yourPrivateKey)
+            
+            let request = self.iconService.sendTransaction(signedTransaction: signed)
+            let response = request.execute()
+            switch response {
+            case .success(let result):
+                print("Message tx result - \(result)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        } catch {
+            print(error)
+        }
+        
+        
+    }
+    
+    // Example of Asynchrously query
+    func asyncBlock() {
+        iconService.getLastBlock().async { (result) in
+            switch result {
+            case .success(let val):
+                print(val.blockHash)
+            case .failure(let err):
+                print(err)
+            }
+            
         }
     }
     
-    func getTransactionByHash() {
-        let response = iconService.getTransaction(hash: "0x1155fd70a265db32f2fa307dd64fa74820dfada2969da6ca7ea00242e319e067").execute()
-        
-        switch response {
-        case .success(let result):
-            print(result.blockHash)
-        case .failure(let err):
-            print(err.errorDescription)
+    func asyncSupply(_ completion: @escaping(BigUInt) -> Void) {
+        iconService.getTotalSupply().async { (result) in
+            switch result {
+            case .success(let val):
+                print(val)
+                completion(val)
+            case .failure(let err):
+                print(err)
+            }
         }
     }
     
-    func test() {
-        let ADDR_SSX_FROM = "hx07abc7a5b8a4941fc0b6930c88b462995acf929b"
-        let ADDR_SSX_SCORE = "cxc72a0973680d0eea29867dfcf48879372ec12d73"
-        let ICON_SERVICE_URL = "https://bicon.net.solidwallet.io/api/v3"
+    func asyncTransaction() {
+        let message = MessageTransaction()
+            .from(fromAddress)
+            .to(toAddress)
+            .nid(self.iconService.nid)
+            .message("Hello, ICON!")
         
-        let iconService = ICONService(provider: ICON_SERVICE_URL, nid: "0x3")
+        let messageRequest = iconService.estimateStep(transaction: message)
+        let messageResponse = messageRequest.execute()
         
-        /* Address의 ICX Value값 나옴.
-         let request: Request<BigUInt> = iconService.getBalance(address: address)
-         let response = request.execute()
-         switch response {
-         case .success(let data):
-         print(data)
-         let resultValue = response.value
-         
-         return SSXAmount(amount: resultValue!)
-         case .failure(let err):
-         print(err)
-         }
-         
-         return nil
-         */
+        if let estimatedStepCost = try? messageResponse.get() {
+            print("stepCost: \(estimatedStepCost)")
+            message.stepLimit(estimatedStepCost)
+        }
         
-        let call = Call<ICONKit.Response.Call<String>>(from: ADDR_SSX_FROM, to: ADDR_SSX_SCORE, method: "balanceOf", params: ["_owner": ADDR_SSX_FROM] )
-        let request:Request<Response.Call> = iconService.call(call)
-        let response = request.execute()
-        
-        guard let value = response.value, let balance = value.result else { return }
-        
-        print("balance : \(balance)")
+        do {
+            let signed = try SignedTransaction(transaction: message, privateKey: yourPrivateKey)
+            let request = iconService.sendTransaction(signedTransaction: signed)
+            
+            request.async { (result) in
+                switch result {
+                case .success(let val):
+                    print(val)
+                case .failure(let err):
+                    print(err)
+                }
+            }
+        } catch {
+            print(error)
+        }
     }
 }
+
